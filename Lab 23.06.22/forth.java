3c110f25b343e3d2ee8a2d0c7a45f0dd09301538
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-06-23 18:52:08 +0530

    Update forth.java

--- Lab 23.06.22/forth.java
+++ Lab 23.06.22/forth.java
@@ -1,14 +1,9 @@
-// multiply two numbers
+// multiply of two numbers
 
-public class forth{
-	int num1,nu2;
-	
-	public complexNum(int x, int y){
-		this.num1 = x;
-		this.num2 = y;
+public class second{
+	public static void main(String[] args){
+		int num1 = 46, num2 = 2;  
+		System.out.print("The multiply of two number is ");
+		System.out.println(num1 * num2);
 	}
-	public void main(){
-		System.out.println(this.num1 + "i25" + this.num2);
-	}
-	public complexNum add(num1,num2);
 }
