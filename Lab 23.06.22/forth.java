f25c53ddae96f2716d81dceda163ae409de20f6d
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-06-23 18:50:58 +0530

    Add files via upload

---
+++ Lab 23.06.22/forth.java
@@ -0,0 +1,14 @@
+// multiply two numbers
+
+public class forth{
+	int num1,nu2;
+	
+	public complexNum(int x, int y){
+		this.num1 = x;
+		this.num2 = y;
+	}
+	public void main(){
+		System.out.println(this.num1 + "i25" + this.num2);
+	}
+	public complexNum add(num1,num2);
+}
