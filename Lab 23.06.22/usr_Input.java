6c01026dbefa92d19ed3b4ef8d79fc4090d8c70a
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-06-24 11:07:01 +0530

    Update usr_Input.java

--- Lab 23.06.22/usr_Input.java
+++ Lab 23.06.22/usr_Input.java
@@ -1,10 +1,14 @@
+/*
+@Author Sankhajit Roy
+*/
+
 // get input from user
-import java.util.Scanner;
+import java.util.Scanner;  // use Scanner util package for user input
 class usr_Input{
-	public static void main(String[] args){
-		Scanner name1 = new Scanner(System.in);
-		System.out.print("Enter name: ");
-		String name2 = name1.nextLine();
-        System.out.println("My name is "+name2);		
+	public static void main(String[] args){   // main method
+		Scanner name1 = new Scanner(System.in);   // create an Scanner object
+		System.out.print("Enter name: ");  
+		String name2 = name1.nextLine();   // read user input
+        System.out.println("My name is "+name2);   // output of user input		
 	}
 }
