90521228d69d052e9938138a69ec608cf301ebf8
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-06-24 11:00:04 +0530

    Update second.java

--- Lab 23.06.22/second.java
+++ Lab 23.06.22/second.java
@@ -1,9 +1,13 @@
+/*
+@Author Sankhajit Roy
+*/
+
 // Addition of two numbers
 
 public class second{
 	public static void main(String[] args){
-		int num1 = 46, num2 = 52;  
-		System.out.print("The addition of two number is ");
-		System.out.println(num1 + num2);
+		int num1 = 46, num2 = 52;   // took two values
+		System.out.print("The addition of two number is ");   // use to print output
+		System.out.println(num1 + num2);  // use to print output
 	}
 }
