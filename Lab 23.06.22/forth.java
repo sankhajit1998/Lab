e658c137728dbab45c1eae9e40469645dbe74250
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-06-24 11:09:07 +0530

    Update forth.java

--- Lab 23.06.22/forth.java
+++ Lab 23.06.22/forth.java
@@ -8,6 +8,6 @@ public class forth{
 	public static void main(String[] args){
 		int num1 = 46, num2 = 2;   // took two values
 		System.out.print("The multiply of two number is ");  // use to print output
-		System.out.println(num1 * num2);  // use to print output
+		System.out.println(num1 * num2);  // use to print output value
 	}
 }
