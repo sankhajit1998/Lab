46b26474bacb4784a9869df6558777021132c914
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-06-24 11:08:44 +0530

    Update first.java

--- Lab 23.06.22/first.java
+++ Lab 23.06.22/first.java
@@ -7,7 +7,7 @@
 public class first{
 	public static void main(String[] args){   
 		float num1 = 75.25f, num2 = 5.2f;   // took two values
-		System.out.print("The value of two number is ");  // use for printing output
-		System.out.println(num1 * num2);  // use for printing output
+		System.out.print("The value of two number is ");  // use to print output
+		System.out.println(num1 * num2);  // use to print output value
 	}
 }
