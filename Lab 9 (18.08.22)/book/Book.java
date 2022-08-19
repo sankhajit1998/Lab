bbd1b06cabd4aae33f755c0fc748bcb5593f29e3
Sankhajit Roy <93027838+sankhajit1998@users.noreply.github.com>
2022-08-19 12:04:27 +0530

    Add files via upload

---
+++ Lab 9 (18.08.22)/book/Book.java
@@ -0,0 +1,59 @@
+package com.book;
+
+public class Book {    // class
+	
+	// took members
+	String bookID, title, author, category;
+	float price;
+	
+	public Book (
+			String bookID,
+			String title,
+			String author,
+			String category,
+			float price)     // parameterized constructor
+	{
+		this.title = title;
+		this.author = author;
+		
+		// try block
+		try {
+			// operation for book categories
+			if (category.equals("Science") || category.equals("Fiction") || category.equals("Technology") || category.equals("Others")) {
+				this.category = category;
+			}
+			else {
+				throw new InvalidBookException();
+			}
+			// operation for book price
+			if(price>0) {
+				this.price = price;
+			}
+			else {
+				throw new InvalidBookException();
+			}
+			
+			// operation for book id
+			if((bookID.length()==4) && (bookID.charAt(0) == 'B')) {
+				this.bookID = bookID;
+			}
+			else {
+				throw new InvalidBookException();
+			}
+			
+		// catch block	
+		} catch (Exception e) {
+			
+		}
+		
+	}
+	
+}
+// create a custom exception class
+class InvalidBookException extends Exception{
+	
+}
+
+
+
+
---
+++ Lab 9 (18.08.22)/book/BookStore.java
@@ -0,0 +1,47 @@
+package com.book;
+
+import java.util.ArrayList;
+
+public class BookStore {     // class 
+	// created a ArrayList object
+	ArrayList<Book> book = new ArrayList<>();
+	
+	// method to add books
+	public void addBook(Book b) {    
+		book.add(b);
+	}
+	
+	// method to search book by title
+	public void searchByTitle(String title) { 
+		// traversing
+		for (Book b: book) {
+			// condition
+			if(title.contains(b.title)) {		
+				System.out.println("Book ID="+b.bookID+ " Title="+b.title+" Author="+b.author+" Category="+b.category+ " Price="+b.price);
+			}else {
+				System.out.println("Element not found");
+			}
+		}
+	}
+	
+	// method to search book by author
+	public void searchByAuthor(String author) {
+		// traversing
+		for (Book b: book) {
+			// condition
+			if(author.contains(b.author)) {			//condition
+				System.out.println("Book ID="+b.bookID+ " Title="+b.title+" Author="+b.author+" Category="+b.category+ " Price="+b.price); 
+			}else {
+				System.out.println("Element not found");
+			}
+		}
+	}
+	
+	// method to display all elements
+	public void displayAll() {
+		// traversing
+		for (Book b: book) {
+			System.out.println("Book ID="+b.bookID+ " Title="+b.title+" Author="+b.author+" Category="+b.category+ " Price="+b.price);
+		}
+	}
+}
